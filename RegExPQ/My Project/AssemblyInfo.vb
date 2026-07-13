cf7939ebfbe593981b99f4292da7eadd95e58aad
Ralf Stolzenburg <41778819+rstsu@users.noreply.github.com>
2026-07-13 12:00:52 +0200

    Update gitignore

---
+++ RegExPQ/My Project/AssemblyInfo.vb
@@ -0,0 +1,46 @@
+﻿Imports System.Resources
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+Imports System.Security
+
+' Allgemeine Informationen über eine Assembly werden über die folgende 
+' Attributgruppe gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+' die einer Assembly zugeordnet sind.
+
+' Werte der Assemblyattribute überprüfen
+
+<Assembly: AssemblyTitle("RegExPQ")>
+<Assembly: AssemblyDescription("Demo Beispiele für Power Query, RegEx, VBA und Formeln...")>
+<Assembly: AssemblyCompany("Ralf Stolzenburg")>
+<Assembly: AssemblyProduct("RegExPQ")>
+<Assembly: AssemblyCopyright("Ralf Stolzenburg (Case) Copyright ©  2026")>
+<Assembly: AssemblyTrademark("")>
+
+' Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+' für COM-Komponenten.  Wenn Sie auf einen Zugriffstyp in dieser Assembly aus 
+' COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+<Assembly: ComVisible(False)>
+
+'Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird.
+<Assembly: Guid("0e740f6e-a937-4ac9-bee0-c50c8d515685")>
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Hauptversion
+'      Nebenversion 
+'      Buildnummer
+'      Revision
+'
+' Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+' übernehmen, indem Sie "*" eingeben:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: NeutralResourcesLanguage("de-DE")>
+Friend Module DesignTimeConstants
+    Public Const RibbonTypeSerializer As String = "Microsoft.VisualStudio.Tools.Office.Ribbon.Serialization.RibbonTypeCodeDomSerializer, Microsoft.VisualStudio.Tools.Office.Designer, Version=10.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a"
+    Public Const RibbonBaseTypeSerializer As String = "System.ComponentModel.Design.Serialization.TypeCodeDomSerializer, System.Design"
+    Public Const RibbonDesigner As String = "Microsoft.VisualStudio.Tools.Office.Ribbon.Design.RibbonDesigner, Microsoft.VisualStudio.Tools.Office.Designer, Version=10.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a"
+End Module
